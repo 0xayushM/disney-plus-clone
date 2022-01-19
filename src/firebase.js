12e690c97df88ec36eecb3d5b900540dfa65a7ac
AyushMangal4982 <mangal.ayush.4982@gmail.com>
2022-01-19 13:24:28 +0530

    enter

---
+++ src/firebase.js
@@ -0,0 +1,25 @@
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/auth'
+import 'firebase/firestore'
+import {initializeApp} from 'firebase/app';
+import {getAuth} from 'firebase/auth'
+import { getFirestore } from '@firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBUBF1CWt5AE2vOFI3p1RMWzNlemY7gR2M",
+    authDomain: "disney-plus-clone-7ebc8.firebaseapp.com",
+    projectId: "disney-plus-clone-7ebc8",
+    storageBucket: "disney-plus-clone-7ebc8.appspot.com",
+    messagingSenderId: "457258204491",
+    appId: "1:457258204491:web:3d406591fcac1c4e127cce",
+    measurementId: "G-WMSQMGKY0R"
+  };
+
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore();
+const auth = firebase.auth();
+// const provider = new GoogleAuthProvider();
+const storage = firebase.storage();
+export const authentication = getAuth();
+// export { auth,provider,storage };
+export default db;
